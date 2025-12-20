2f03ba3830de692fa5cc8f4ed237cab5a8a805ee
karpagalakshmi20 <karpagalakshmipandian20@gmail.com>
2025-12-20 13:47:27 +0530

    Printing 9th table - v1

---
+++ src/task/Task_Dec17_Table.java
@@ -0,0 +1,12 @@
+package task;
+
+public class Task_Dec17_Table {
+    public static void main(String args[]){
+        int a = 9, n=1;
+        while(n<=10){
+            System.out.printf("%d x %d = %d\n", a, n, a*n);
+            n++;
+        }
+
+    }
+}
