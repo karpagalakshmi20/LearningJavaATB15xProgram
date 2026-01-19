7334bd0d88b0fb3386832c17f1b420223a0e5346
karpagalakshmi20 <karpagalakshmipandian20@gmail.com>
2026-01-19 13:24:09 +0530

    Exam 18th Jan - CheckStringLength - v1

---
+++ src/ATB15XProgram_Exam_16_Jan_2026/CheckStringLength.java
@@ -0,0 +1,13 @@
+package ATB15XProgram_Exam_16_Jan_2026;
+import java.util.*;
+
+public class CheckStringLength {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an input String:");
+        String s1 = sc.nextLine();
+        char[] chars = s1.toCharArray();
+        int len = chars.length;
+        System.out.println((len>10) ? "Yes, length is greater than 10. " + "The length is:" + len : "No, length is not greater than 10. " + "The length is:" + len);
+    }
+}
